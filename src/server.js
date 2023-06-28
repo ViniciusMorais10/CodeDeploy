d5e90e14a8bc35e9a06723b890f7b8a43c2db8e7
Vinicius de Morais <vmorais@fasthelp.inf.br>
2023-06-28 16:12:43 -0300

    teste deploy

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+
+app.get('/deploy', (request, response) => {
+  response.json({ message: 'Hello AWS!' });
+})
+
+app.listen(3000, () => {
+  console.log("Server is runnig");
+});
